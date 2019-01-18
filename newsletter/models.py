4a309dce165cfce6b50eef28dfc9893163e879ca
carlosribas <caduribas@gmail.com>
2019-01-18 15:53:20 -0200

    Filtrando posts apenas pela categoria

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.db import models
 from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
@@ -14,42 +12,22 @@ class Newsletter(models.Model):
     number = models.CharField(_('Number'), max_length=100, unique=True)
     date = models.DateField(_('Date'), default=now)
     featured = models.ManyToManyField(
-        BlogPost, blank=True, related_name='featured_post', limit_choices_to={
-            'publish_date__month': datetime.datetime.now().strftime("%m"),
-            'publish_date__year': datetime.datetime.now().strftime("%Y"),
-            'categories__slug': 'newsletter'
-        }
+        BlogPost, blank=True, related_name='featured_post', limit_choices_to={'categories__slug': 'newsletter'}
     )
     publication = models.ManyToManyField(
-        BlogPost, blank=True, related_name='publication_post', limit_choices_to={
-            'publish_date__month': datetime.datetime.now().strftime("%m"),
-            'publish_date__year': datetime.datetime.now().strftime("%Y"),
-            'categories__slug': 'publication'
-        }
+        BlogPost, blank=True, related_name='publication_post', limit_choices_to={'categories__slug': 'publication'}
     )
     in_the_media = models.ManyToManyField(
-        BlogPost, blank=True, related_name='in_the_media_post', limit_choices_to={
-            'publish_date__month': datetime.datetime.now().strftime("%m"),
-            'publish_date__year': datetime.datetime.now().strftime("%Y"),
-            'categories__slug': 'news'
-        }
+        BlogPost, blank=True, related_name='in_the_media_post', limit_choices_to={'categories__slug': 'news'}
     )
     opportunity = models.ManyToManyField(
-        BlogPost, blank=True, related_name='opportunity_post', limit_choices_to={
-            'categories__slug': 'opportunities'
-        }
+        BlogPost, blank=True, related_name='opportunity_post', limit_choices_to={'categories__slug': 'opportunities'}
     )
     streaming = models.ManyToManyField(
-        BlogPost, blank=True, related_name='streaming_post', limit_choices_to={
-            'publish_date__month': datetime.datetime.now().strftime("%m"),
-            'publish_date__year': datetime.datetime.now().strftime("%Y"),
-            'categories__slug': 'streaming'
-        }
+        BlogPost, blank=True, related_name='streaming_post', limit_choices_to={'categories__slug': 'streaming'}
     )
     event = models.ManyToManyField(
-        BlogPost, blank=True, related_name='event_post', limit_choices_to={
-            'categories__slug': 'events'
-        }
+        BlogPost, blank=True, related_name='event_post', limit_choices_to={'categories__slug': 'events'}
     )
 
     def __str__(self):
