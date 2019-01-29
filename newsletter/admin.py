6b430c6a25f3b2b7fdcbf184580d26fc51b06c51
carlosribas <caduribas@gmail.com>
2019-01-29 16:54:20 -0200

    Montrando outras informações no admin para a classe Subscription

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -4,5 +4,13 @@ from .models import Newsletter, Subscription, FacebookHighlight
 
 
 admin.site.register(Newsletter)
-admin.site.register(Subscription)
 admin.site.register(FacebookHighlight)
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    fields = ['email', 'create_date', 'status', 'status_date']
+    list_display = ('email', 'create_date', 'status', 'status_date')
+    search_fields = ['email']
+    list_display_links = ('email',)
+
+admin.site.register(Subscription, SubscriptionAdmin)
