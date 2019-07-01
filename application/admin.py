9a8df8e8b6a74789e8f783656e25d840398115a8
lucazweb <lucazwebmail@gmail.com>
2019-07-01 17:31:41 -0300

    Inserindo campos "Créditos" e "Legenda" para imagens dos posts. Fix, linha 38 no admin.py

--- application/admin.py
+++ application/admin.py
@@ -35,7 +35,7 @@ admin.site.register(Postdoc, PostdocAdmin)
 blog_fieldsets = deepcopy(BlogPostAdmin.fieldsets)
 blog_fieldsets[0][1]["fields"] += ("hide_post", "altmetric_type", "altmetric_number")
 blog_fieldsets[0][1]["fields"].insert(-5, "legend")
-blog_fieldsets[0][1]["fields"].insert(-6, "creditsls")
+blog_fieldsets[0][1]["fields"].insert(-6, "credits")
 
 
 class MyBlogPostAdmin(BlogPostAdmin):
