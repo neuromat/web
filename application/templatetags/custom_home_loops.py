97d927792bd90ca0212a6dc71cb5dc1d8cfff7ee
lucazweb <lucazwebmail@gmail.com>
2019-11-04 13:17:15 -0300

    Transpondo nova camada front-end para o portal / primeiras adaptaões
    * Adiciona custom template tags para carregar loop de notícias;
    * Resolve posts que não tem imagem destacada;
    * Ajustes visuais

--- application/templatetags/custom_home_loops.py
+++ application/templatetags/custom_home_loops.py
@@ -1,6 +1,10 @@
 from django import template
 from html import unescape
 
+from django.db.models import Q
+from mezzanine.accounts.admin import User
+from mezzanine.blog.models import BlogPost, BlogCategory
+from mezzanine.generic.models import Keyword
 from mezzanine.pages.models import Page
 
 register = template.Library()
@@ -30,3 +34,41 @@ def project_swiper_component():
     Provide a slidehow component with main products and internal links.
     """
     pass
+
+
+@register.simple_tag
+def blog_recent_posts(limit=8, tag=None, username=None, category=None):
+    """
+    Put a list of recently published blog posts into the template
+    context. A tag title or slug, category title or slug or author's
+    username can also be specified to filter the recent posts returned.
+
+    Usage::
+
+        {% blog_recent_posts 5 as recent_posts %}
+        {% blog_recent_posts limit=5 tag="django" as recent_posts %}
+        {% blog_recent_posts limit=5 category="python" as recent_posts %}
+        {% blog_recent_posts 5 username=admin as recent_posts %}
+
+    """
+    blog_posts = BlogPost.objects.published().select_related("user")
+    title_or_slug = lambda s: Q(title=s) | Q(slug=s)
+    if tag is not None:
+        try:
+            tag = Keyword.objects.get(title_or_slug(tag))
+            blog_posts = blog_posts.filter(keywords__keyword=tag)
+        except Keyword.DoesNotExist:
+            return []
+    if category is not None:
+        try:
+            category = BlogCategory.objects.get(title_or_slug(category))
+            blog_posts = blog_posts.filter(categories=category)
+        except BlogCategory.DoesNotExist:
+            return []
+    if username is not None:
+        try:
+            author = User.objects.get(username=username)
+            blog_posts = blog_posts.filter(user=author)
+        except User.DoesNotExist:
+            return []
+    return list(blog_posts[:limit])
